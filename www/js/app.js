4718ff79561e9f69ca9990dcf0c6d7e31d92e8e4
bmunir <basitmunir147@gmail.com>
2019-11-11 10:25:48 +0500

    Enabled various barcodes

--- www/js/app.js
+++ www/js/app.js
@@ -61,14 +61,14 @@
 							  alert("Scanning failed: " + error);
 						  },
 						  {
-							  preferFrontCamera : true, // iOS and Android
+							  preferFrontCamera : false, // iOS and Android
 							  showFlipCameraButton : true, // iOS and Android
 							  showTorchButton : true, // iOS and Android
 							  torchOn: true, // Android, launch with the torch switched on (if available)
 							  saveHistory: true, // Android, save scan history (default false)
 							  prompt : "Place a barcode inside the scan area", // Android
 							  resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
-							  formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+							  formats : "QR_CODE,PDF_417,DATA_MATRIX,UPC_A,UPC_E, EAN_13, CODE_39, CODE_93, CODE_128, CODABAR, ITF, RSS14, RSS_EXPANDED, MSI, AZTEC", // default: all but PDF_417 and RSS_EXPANDED
 							  orientation : "landscape", // Android only (portrait|landscape), default unset so it rotates with the device
 							  disableAnimations : true, // iOS
 							  disableSuccessBeep: false // iOS and Android
